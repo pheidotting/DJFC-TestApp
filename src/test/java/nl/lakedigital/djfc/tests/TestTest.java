a21c58027f719482e7bad284e86d8932fdd9428c
Patrick Heidotting <patrick@lakedigital.nl>
2017-01-06 18:41:59 +0100

    phantomjs linux

--- src/test/java/nl/lakedigital/djfc/tests/TestTest.java
+++ src/test/java/nl/lakedigital/djfc/tests/TestTest.java
@@ -2,16 +2,12 @@ package nl.lakedigital.djfc.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 
 import static com.codeborne.selenide.Selenide.close;
-import static com.codeborne.selenide.Selenide.open;import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 
 public class TestTest {
@@ -19,7 +15,7 @@ public class TestTest {
     public void testGoogle(){
         Configuration.reportsFolder = "target/screenshots";
 
-        System.setProperty("phantomjs.binary.path", "src/test/resources/phantomjs");
+        System.setProperty("phantomjs.binary.path", "src/test/resources/phantomjs-linux");
 
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
         System.setProperty("phantomjs.binary.path", "src/test/resources/phantomjs");
